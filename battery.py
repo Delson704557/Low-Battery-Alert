9a3a746fbe36a6ea646e7ce5f668a4ba26805319
Delson704557 <46827625+Delson704557@users.noreply.github.com>
2020-04-10 20:41:29 +0530

    Update battery.py

--- battery.py
+++ battery.py
@@ -5,8 +5,7 @@ power_now = open("/sys/class/power_supply/BAT1/charge_now", "r").readline()
 power_full = open("/sys/class/power_supply/BAT1/charge_full", "r").readline()
 total_battery = (float(power_now)/float(power_full))*100
 print (float(total_battery))
-while total_battery <= 90:
-	print("")
+while total_battery <= 110:
 	power_now = open("/sys/class/power_supply/BAT1/charge_now", "r").readline()
 	power_full = open("/sys/class/power_supply/BAT1/charge_full", "r").readline()
 	total_battery = (float(power_now)/float(power_full))*100

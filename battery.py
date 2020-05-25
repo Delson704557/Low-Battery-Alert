c18cc569a8799978d5c25e5eae66746e2ce8b4af
Delson704557 <46827625+Delson704557@users.noreply.github.com>
2020-05-25 08:38:05 +0530

    Update battery.py

--- battery.py
+++ battery.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python
-# By Delson
+# Written by Delson
 import time
 import notify2
 from playsound import playsound
-power_now = open("/sys/class/power_supply/BAT1/charge_now", "r").readline()
-power_full = open("/sys/class/power_supply/BAT1/charge_full", "r").readline()
-total_battery = (float(power_now)/float(power_full))*100
-battery=int(total_battery)
-print (float(total_battery))
-while total_battery <= 110:
+flag=True
+while flag:
 	power_now = open("/sys/class/power_supply/BAT1/charge_now", "r").readline()
 	power_full = open("/sys/class/power_supply/BAT1/charge_full", "r").readline()
 	total_battery = (float(power_now)/float(power_full))*100
-	time.sleep(90)
-	if total_battery <= 20:
-		notify2.URGENCY_CRITICAL
-		notify2.init('')
-		n = notify2.Notification('critical battery' , 'Kindly put it on Charge')
-		n.show()
-		notify2.init('critical')
-		n = notify2.Notification('Kindly put it on Charge' , 'CRITICAL BATTERY  ' + str(battery) + '%')
+	battery=int(total_battery)
+	print (total_battery)
+	while total_battery <= 110:
+		power_now = open("/sys/class/power_supply/BAT1/charge_now", "r").readline()
+		power_full = open("/sys/class/power_supply/BAT1/charge_full", "r").readline()
+		total_battery = (float(power_now)/float(power_full))*100
+		time.sleep(70)
+		if total_battery <= 20:
+			notify2.URGENCY_CRITICAL
+			notify2.init('critical')
+			n = notify2.Notification('Kindly put it on Charge' , 'CRITICAL BATTERY  ' + str(battery) + '%')
+			n.show()
+			playsound('/root/Desktop/battery/battery.mp3')
 
-		n.show()
-		playsound('bell.mp3')
-		quit()
-  

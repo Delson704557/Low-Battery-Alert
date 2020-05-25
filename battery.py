35d40e9a7d9b0a3c2f9ca3cff91f91c4e10450c6
Delson704557 <46827625+Delson704557@users.noreply.github.com>
2020-05-25 08:41:50 +0530

    Update battery.py

--- battery.py
+++ battery.py
@@ -5,6 +5,7 @@ import notify2
 from playsound import playsound
 flag=True
 while flag:
+	time.sleep(200)
 	power_now = open("/sys/class/power_supply/BAT1/charge_now", "r").readline()
 	power_full = open("/sys/class/power_supply/BAT1/charge_full", "r").readline()
 	total_battery = (float(power_now)/float(power_full))*100
